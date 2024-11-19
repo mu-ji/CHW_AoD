0face89630a4541bb331584fb96f7709ffe122c3
mu-ji <1142260696@qq.com>
2024-11-19 13:50:59 +0800

    debugs

--- three_ant_receiver.py
+++ three_ant_receiver.py
@@ -31,7 +31,6 @@ while True:
     byte  = ser.read(1)        
     rawFrame += byte
     #print(len(rawFrame))
-
     if rawFrame[-3:]==[255, 255, 255]:
         if len(rawFrame) == 4*num_samples+8:
             received_data = rawFrame[:4*num_samples]
@@ -49,55 +48,10 @@ while True:
                 Q_data[i] = Q[0]
 
             I_data = I_data.astype(np.float32)
-            print(I_data)
             Q_data = Q_data.astype(np.float32)
+
             all_data['I_data'].append(I_data)
             all_data['Q_data'].append(Q_data)
-
-            plt.figure()
-            antenna_samples = (num_samples-8)/8       #3 is number of antennas
-
-            
-            ax1 = plt.subplot(231)
-            i = 0
-            while i < 8:
-                ax1.plot([0,I_data[i]], [0, Q_data[i]], label='{}'.format(i))
-                i = i+1
-            plt.legend()
-            #after the while, i = 8 which means switch slot
-            
-            ax2 = plt.subplot(232)      #ax2 is the phase calculated by IQ data
-            ax2.scatter([i for i in range(72)], [64*np.arctan(Q_data[i]/I_data[i]) for i in range(72)])
-            plt.legend()
-
-            i = 9
-            ax3 = plt.subplot(233)      # ax3 is the pattern of antenna 0
-            ax3.set_title('antenna 0 data')
-            plt.legend()
-            ax4 = plt.subplot(234)      # ax4 is the pattern of antenna 1
-            ax4.set_title('antenna 1 data')
-            plt.legend()
-            ax5 = plt.subplot(235)      # ax5 is the pattern of antenna 2
-            ax5.set_title('antenna 2 data')
-            plt.legend()
-            ax6 = plt.subplot(236)
-            ax6.set_title('antenna 3 data')
-            
-            times = 1
-            while i < num_samples and times < antenna_samples:
-                ax3.plot([0,I_data[i]], [0, Q_data[i]], label='{}'.format(i))
-                i = i + 2
-                ax4.plot([0,I_data[i]], [0, Q_data[i]], label='{}'.format(i))
-                i = i + 2
-                ax5.plot([0,I_data[i]], [0, Q_data[i]], label='{}'.format(i))
-                i = i + 2
-                ax6.plot([0,I_data[i]], [0, Q_data[i]], label='{}'.format(i))
-                i = i + 2
-                times = times + 1
-
-            plt.legend()
-            
-            plt.show()
 
             reference_I = I_data[:8]
             reference_Q = Q_data[:8]
@@ -136,13 +90,6 @@ while True:
             ant2_I, ant2_Q = ant_IQ_norm(ant2_I, ant2_Q)
             ant3_I, ant3_Q = ant_IQ_norm(ant3_I, ant3_Q)
             
-            for i in range(len(ant0_I)):
-                plt.plot([0,ant0_I[i]], [0, ant0_Q[i]], label='{}'.format(i))
-                i = i+1
-            plt.legend()
-            plt.title('ant0 IQ vector after normalization')
-            plt.show()
-            
             #from the reference IQ vector pattern, there is a small change between every us except pi/2 caused by 250KHz frequency shift
 
             def calculate_angle(I1, Q1, I2, Q2):
@@ -154,7 +101,7 @@ while True:
                 
                 # 计算角度 (弧度)
                 theta = np.arccos(cos_theta)
-                
+
                 # 返回角度（可选：将弧度转为角度）
                 angle_in_degrees = np.degrees(theta)
                 
@@ -178,35 +125,7 @@ while True:
                     rotate_angle = i*(3.14/2 + angle_change_1us)*8
                     ant_I[i], ant_Q[i] = rotate_vector(ant_I[i], ant_Q[i], -rotate_angle)
                 return ant_I, ant_Q
-            
-            plt.figure()
-            ax1 = plt.subplot(121)
-            unit_circle = plt.Circle((0, 0), 1, color='blue', fill=False, label='Unit Circle')
-            ax1.add_artist(unit_circle)
-            for i in range(len(ant0_I)):
-                ax1.plot([0,ant0_I[i]], [0, ant0_Q[i]], label='{}'.format(i))
-                i = i+1
-            ax1.set_title('ant0 IQ vector before compensation')
-            ax1.set_xlim(-1,1)
-            ax1.set_ylim(-1,1)
-            plt.legend()
-            
-            ant0_I, ant0_Q = compensate_phase(ant0_I, ant0_Q, angle_change_1us)
-            ant1_I, ant1_Q = compensate_phase(ant1_I, ant1_Q, angle_change_1us)
-            ant2_I, ant2_Q = compensate_phase(ant2_I, ant2_Q, angle_change_1us)
-            ant3_I, ant3_Q = compensate_phase(ant3_I, ant3_Q, angle_change_1us)
-            
-            ax2 = plt.subplot(122)
-            unit_circle = plt.Circle((0, 0), 1, color='blue', fill=False, label='Unit Circle')
-            ax2.add_artist(unit_circle)
-            for i in range(len(ant0_I)):
-                ax2.plot([0,ant0_I[i]], [0, ant0_Q[i]], label='{}'.format(i))
-                i = i+1
-            ax2.set_title('ant0 IQ vector after compensation')
-            ax2.set_xlim(-1,1)
-            ax2.set_ylim(-1,1)
-            plt.legend()
-            plt.show()
+
             
             
             ant0_I_mean, ant0_Q_mean = np.mean(ant0_I), np.mean(ant0_Q)
@@ -226,32 +145,7 @@ while True:
             ant1_I_mean, ant1_Q_mean = normalization(ant1_I_mean, ant1_Q_mean)
             ant2_I_mean, ant2_Q_mean = normalization(ant2_I_mean, ant2_Q_mean)
             ant3_I_mean, ant3_Q_mean = normalization(ant1_I_mean, ant3_Q_mean)
-
-            
-            ax = plt.subplot(111)
-            k = 0
-            #ax.plot([0, np.min(ant0_I)], [0, ant0_Q[np.argmin(ant0_I)]], c = 'b', linewidth = 1, label = 'ant0_min')
-            #ax.plot([0, np.max(ant0_I)], [0, ant0_Q[np.argmax(ant0_I)]], c = 'b', linewidth = 3, label = 'ant0_max')
-            ax.plot([0, ant0_I_mean], [0, ant0_Q_mean], c = 'b', linestyle='--', label = 'ant0_mean')
-            #ax.plot([0, ant0_I[k]], [0, ant0_Q[k]], c = 'b', linestyle='--', label = 'ant0_mean')
-            #ax.plot([0, np.min(ant1_I)], [0, ant1_Q[np.argmin(ant1_I)]], c = 'y', linewidth = 1, label = 'ant1_min')
-            #ax.plot([0, np.max(ant1_I)], [0, ant1_Q[np.argmax(ant1_I)]], c = 'y', linewidth = 3, label = 'ant1_max')
-            ax.plot([0, ant1_I_mean], [0, ant1_Q_mean], c = 'y', linestyle='--', label = 'ant1_mean')
-            #ax.plot([0, ant1_I[k]], [0, ant1_Q[k]], c = 'y', linestyle='--', label = 'ant1_mean')
-            #ax.plot([0, np.min(ant2_I)], [0, ant2_Q[np.argmin(ant2_I)]], c = 'g', linewidth = 1, label = 'ant2_min')
-            #ax.plot([0, np.max(ant2_I)], [0, ant2_Q[np.argmax(ant2_I)]], c = 'g', linewidth = 3, label = 'ant2_max')
-            ax.plot([0, ant2_I_mean], [0, ant2_Q_mean], c = 'g', linestyle='--', label = 'ant2_mean')
-            #ax.plot([0, ant2_I[k]], [0, ant2_Q[k]], c = 'g', linestyle='--', label = 'ant2_mean')
-            ax.plot([0, ant3_I_mean], [0, ant3_Q_mean], c = 'pink', linestyle='--', label = 'ant3_mean')
-
-            unit_circle = plt.Circle((0, 0), 1, color='blue', fill=False, label='Unit Circle')
-            ax.add_artist(unit_circle)
-            ax.set_xlim(-1,1)
-            ax.set_ylim(-1,1)
-            plt.legend()
-            plt.show()
-            
-
+        
             #for i in range(len(ant0_I)):
 
             def steering_vector(alpha):
@@ -262,48 +156,28 @@ while True:
                 ant0_theta = cmath.phase(complex(ant0_I_mean, ant0_Q_mean))
                 ant1_theta = cmath.phase(complex(ant1_I_mean, ant1_Q_mean))
                 ant2_theta = cmath.phase(complex(ant2_I_mean, ant2_Q_mean))
-                
+                ant3_theta = cmath.phase(complex(ant3_I_mean, ant3_Q_mean))
+
                 ant1_theta = ant1_theta - ant0_theta
                 ant2_theta = ant2_theta - ant0_theta
+                ant3_theta = ant3_theta - ant0_theta
                 ant0_theta = 0
-                #print(ant0_theta, ant1_theta, ant2_theta)
+
                 received_signal = np.array([cmath.exp(1j*ant0_theta), cmath.exp(1j*ant1_theta), cmath.exp(1j*ant2_theta)])
-                print(received_signal)
+               
                 angle_list = [np.radians(i) for i in range(-90, 90)]
                 y_alpha_list = []
                 for alpha in angle_list:
                     y_alpha = steering_vector(alpha)[0]*received_signal[0] + steering_vector(alpha)[1]*received_signal[1] + steering_vector(alpha)[2]*received_signal[2]
                     y_alpha_list.append(y_alpha)
-
+                #print(y_alpha_list)
                 #plt.plot([i for i in range(-90, 90)], y_alpha_list)
                 #plt.show()
+
                 return [i for i in range(-90, 90)][np.argmax(np.array(y_alpha_list))]
             
             angle = DoA_algorithm(ant0_I_mean, ant0_Q_mean, ant1_I_mean, ant1_Q_mean, ant2_I_mean, ant2_Q_mean)
             print('DoA:', angle)
-
-            reference_I = I_data[:8]
-            reference_Q = Q_data[:8]
-
-            ant0_I = I_data[9:88:8]
-            ant0_Q = Q_data[9:88:8]
-
-            ant1_I = I_data[11:88:8]
-            ant1_Q = Q_data[11:88:8]
-
-            ant2_I = I_data[13:88:8]
-            ant2_Q = Q_data[13:88:8]
-
-            ant3_I = I_data[15:88:8]
-            ant3_Q = Q_data[15:88:8]
-
-            rssi = bytes(rawFrame[-8:-4])
-            rssi = int(rssi.decode('utf-8'))
-            #print(iteration)
-            all_data['rssi'].append(rssi)
-            print(iteration, rssi, len(all_data['rssi']))
-            #print('packet_number:',rawFrame[-4])
-            #print('-------------------------------')
 
             
         rawFrame = []
