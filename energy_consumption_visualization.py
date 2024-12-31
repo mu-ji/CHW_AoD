4a762695ce80850100b6b4b5beede3387d6bd7e4
mu-ji <1142260696@qq.com>
2024-12-31 13:20:53 +0800

    update energy consumption visualization

--- energy_consumption_visualization.py
+++ energy_consumption_visualization.py
@@ -49,15 +49,28 @@ plt.legend()
 plt.grid()
 plt.show()
 
-aod_consumption = (160e-6*6e-3 + 184e-6*6e-3 + 53e-3*3.1e-3 + (500e-3 - 53e-3 - 160e-6 - 184e-6)*1.8e-6)/500e-3
+aod_consumption = (160e-6*6e-3 + 184e-6*6e-3 + 14e-3*3.1e-3 + (500e-3 - 14e-3 - 160e-6 - 184e-6)*1.8e-6)/500e-3
 print(aod_consumption)
 
 def aoa_consumption(n):
     return (168e-6*6e-3 + (10+3*n)*8e-6*6e-3 + (500e-3 - 168e-6 - (10+3*n)*8*1e-6)*1.8e-6)/500e-3
 print(aoa_consumption(1))
 
-node_number = range(1, 2000, 1)
+node_number = range(1, 500, 1)
 plt.plot(node_number, [aod_consumption]*len(node_number), label='AoD node current electricity(uA)')
 plt.plot(node_number, [aoa_consumption(i) for i in node_number], label='AoA node current electricity(uA)')
+plt.grid()
 plt.legend()
 plt.show()
+
+aoa_node_mean_consumption = aoa_node_data['Current(uA)'].mean()
+aoa_anchor_mean_consumption = aoa_anchor_data['Current(uA)'].mean()
+aod_node_mean_consumption = aod_node_data['Current(uA)'].mean()   
+aod_anchor_consumption = aod_anchor_data['Current(uA)'].mean()
+aod_node_without_network_consumption = aod_node_without_network_data['Current(uA)'].mean()
+
+print('aoa_node_mean_consumption:', aoa_node_mean_consumption)
+print('aoa_anchor_mean_consumption:', aoa_anchor_mean_consumption)
+print('aod_node_mean_consumption:', aod_node_mean_consumption)
+print('aod_anchor_consumption:', aod_anchor_consumption)
+print('aod_node_without_network_consumption:', aod_node_without_network_consumption)
